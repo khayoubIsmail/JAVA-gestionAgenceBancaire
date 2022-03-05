85bca996584985b47fddb4acf5699decd5730b59
khayoubIsmail <95181275+khayoubIsmail@users.noreply.github.com>
2022-03-06 00:01:40 +0100

    Add files via upload
    Le Rapport sera lancé bientôt

---
+++ GestionAgenceBancaire/src/Directeur.java
@@ -0,0 +1,16 @@
+
+public class Directeur extends Employe{
+	
+	public Directeur() {}
+	
+	public Directeur(String _code,String _nom , String _prenom , String _ville,String _fonction)
+	{
+		super(_code,_nom,_prenom,_ville,_fonction);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return super.toString();
+	}
+}
